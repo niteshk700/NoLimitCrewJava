8589d775fcb07c1235239a58de12abf90c712956
Nitesh Kumar <33413866+niteshk700@users.noreply.github.com>
2023-10-09 20:31:02 +0530

    Create 6. Square root.java

---
+++ 6. Square root.java
@@ -0,0 +1,8 @@
+public class SquareRootExample {
+    public static void main(String[] args) {
+        double number = 16.0; // Change this to the number you want to find the square root of
+        double squareRoot = Math.sqrt(number);
+        
+        System.out.println("The square root of " + number + " is " + squareRoot);
+    }
+}
